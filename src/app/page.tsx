53fe0c83025c1a3816fa15d8a2827a16781c358b
E R I C S C H M I D T <info@ericschmidt.de>
2024-12-30 14:34:43 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,75 @@
+"use client";
+import { Button, Checkbox, Paper } from "@mantine/core";
+import { IconDeviceFloppy } from "@tabler/icons-react";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
-export default function Home() {
+export default function Page() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const [digitalId, setDigitalId] = useState(false);
+  const [invitation, setInvitation] = useState(false);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex justify-center items-center p-8">
+      <Paper
+        p="xl"
+        radius="md"
+        w="666"
+        className="flex flex-col items-start gap-8"
+        withBorder
+      >
+        <header className="w-full flex justify-between items-center pb-4">
+          <div className="flex justify-center items-center gap-1 ">
+            <Image src="/logo.svg" alt="1. FCN Logo" width={48} height={48} />
+            <p className="text-2xl">
+              <b>Mitglieder</b>
+              <i>umfrage</i>
+            </p>
+          </div>
+          <p className="muted">2025 Q1</p>
+        </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log(
+              JSON.stringify({ token, digitalId, invitation }, null, 2)
+            );
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          <Checkbox
+            label="Ich möchte den Mitgliedsausweis digital erhalten."
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus, ligula at fermentum egestas, sapien sem placerat tellus, et tristique lacus mauris in purus. Donec ac eros sodales nibh dapibus semper. Aliquam vulputate massa ut metus dapibus ullamcorper. Nunc sed volutpat ex. Aenean et nisl pretium magna auctor porta in ac quam."
+            size="lg"
+            checked={digitalId}
+            onChange={(e) => setDigitalId(e.currentTarget.checked)}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Checkbox
+            label="Ich möchte die Einladung zur Jahreshauptversammlung digital erhalten."
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus, ligula at fermentum egestas, sapien sem placerat tellus, et tristique lacus mauris in purus. Donec ac eros sodales nibh dapibus semper. Aliquam vulputate massa ut metus dapibus ullamcorper. Nunc sed volutpat ex. Aenean et nisl pretium magna auctor porta in ac quam."
+            size="lg"
+            checked={invitation}
+            onChange={(e) => setInvitation(e.currentTarget.checked)}
+          />
+        </form>
+        <p className="small">
+          Ich bin damit einverstanden, dass der 1. Fußball-Club Nürnberg e.V.
+          mir regelmäßig Informationen zu den oben genannten Themen per E-Mail
+          an die angegebene E-Mail-Adresse zukommen lässt. Meine Einwilligung
+          kann ich jederzeit mit Wirkung für die Zukunft widerrufen.
+        </p>
+        <Button
+          type="submit"
+          fullWidth
+          leftSection={<IconDeviceFloppy size={16} />}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Speichern
+        </Button>
+      </Paper>
     </div>
   );
 }
