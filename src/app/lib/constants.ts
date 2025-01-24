d6ae6633bc6ae9f82e64489b9dc18cd1aedc81a2
E R I C S C H M I D T <info@ericschmidt.de>
2025-01-24 14:25:57 +0100

    Update constants.ts

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,2 +1,2 @@
-export const FCN_WEB_API = "https://stage.comoso.biz:8443/FCNWebApi/api/survey";
+export const FCN_WEB_API = "https://webapi.fcn.de:8443/FCNWebApi/api/survey";
 export const SURVEY = "2025Q1";
